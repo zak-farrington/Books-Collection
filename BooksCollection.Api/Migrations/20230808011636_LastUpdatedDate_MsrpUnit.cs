73d753267155620f3447205040ba76b4bc4498aa
zak-farrington <fritz@hax-studios.net>
2023-08-07 21:32:21 -0500

    add ModifyBook model and migrations for LastUpdateDate & MsrpUnit

---
+++ BooksCollection.Api/Migrations/20230808011636_LastUpdatedDate_MsrpUnit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookCollection.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastUpdatedDate_MsrpUnit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedDate",
+                table: "Book",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "MsrpUnit",
+                table: "Book",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedDate",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "MsrpUnit",
+                table: "Book");
+        }
+    }
+}
---
+++ BooksCollection.Api/Models/ModifyBook.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksCollection.Api.Models
+{
+    public class ModifyBookRequest
+    {
+        [Required(ErrorMessage = "Book details are required.")]
+        public Book Book { get; set; }
+    }
+
+    public class ModifyBookResponse : BaseApiResponse
+    {
+
+    }
+}
